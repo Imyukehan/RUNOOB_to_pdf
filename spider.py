e7fb01ee019437428ff5fc9f7ae65fdd1a6b7fbb
Khan <njyukehan@gmail.com>
2017-06-15 13:26:26 +0800

    雏形完成

--- spider.py
+++ spider.py
@@ -1,4 +1,5 @@
 import requests
+import pdfkit
 from bs4 import BeautifulSoup
 
 start_url = 'http://www.runoob.com/php/php-tutorial.html'
@@ -17,19 +18,47 @@ def get_content(url):
     soup = BeautifulSoup(html, 'lxml')
     main_page = soup.find('div', class_='article-intro')
     return main_page
-'''
+
+def save_html(html):
+    with open('index.html', 'a') as f:
+        f.write(str(html))
+
+
+
+def save_pdf(htmls):
+    '''
+    把所有html文件转换成pdf文件
+    '''
+    file_name = 'php.pdf'
+    options = {
+        'page-size': 'Letter',
+        'encoding': "UTF-8",
+        'custom-header': [
+            ('Accept-Encoding', 'gzip')
+        ]
+    }
+    pdfkit.from_file(htmls, file_name, options=options)
+
+times = 1
+
 def traverse(url):
+    print(url)
     html = get_html(url)
     next_url = get_next_url(html)
-    print(next_url)
+    page = get_content(next_url)
+    save_html(page)
+    global times
+    times += 1
+    if times > 70:
+        return
+
     traverse(next_url)
-'''
+
 
 def main():
-#    traverse(start_url)
-    html = get_html(start_url)
-    next_url = get_next_url(html)
-    print(get_content(next_url))
+    #traverse(start_url)
+    save_pdf('index.html')
+    
 
 if __name__ == '__main__':
     main()
